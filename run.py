7c9cda5f6dbb1db3fc1f9b6328afe8d6b6166922
pitufo08 <camilo.vasquez82@gmail.com>
2020-05-22 15:04:22 +0200

    adding a new @app route "careers.html"

--- run.py
+++ run.py
@@ -3,20 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
-
 
 @app.route('/about')
 def about():
     return render_template("about.html")
 
-
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
