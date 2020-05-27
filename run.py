c07d564c42597a82fd24c198d8d102e725b39f33
pitufo08 <camilo.vasquez82@gmail.com>
2020-05-27 10:19:04 +0200

    load data from JSON

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,7 +10,11 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template("about.html" , page_tittle="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html" , page_tittle="About", company=data)
+    
 
 @app.route('/contact')
 def contact():
