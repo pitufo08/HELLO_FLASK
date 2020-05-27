f92325551b8f78071f5e1e94b2c0f9b3683165f7
pitufo08 <camilo.vasquez82@gmail.com>
2020-05-27 09:41:01 +0200

    bug fix run.py file

--- run.py
+++ run.py
@@ -13,11 +13,11 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html , page_tittle="Contact")
+     return render_template("contact.html" , page_tittle="Contact")
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html"page_tittle="Careers")
+     return render_template("careers.html" , page_tittle="Careers")
 
 if __name__ == '__main__':
     app.run(debug=True)
