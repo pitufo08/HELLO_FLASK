019322e2757ab3bdfcd16443f8181bdc19726836
pitufo08 <camilo.vasquez82@gmail.com>
2020-05-28 13:04:37 +0200

    uppdate

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def careers():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=False)
